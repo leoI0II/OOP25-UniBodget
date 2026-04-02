f87caaffd7bf662fdc7623f8234f96d70b8487e3
Arianna Palmucci <arianna.palmucci@studio.unibo.it>
2026-04-02 20:25:27 +0200

    add dataserializer interface definition and implemented dataserializerexception class for serializing error handling

--- app/src/main/java/it/unibo/unibodget/persistency/parser/api/DataSerializer.java
+++ app/src/main/java/it/unibo/unibodget/persistency/parser/api/DataSerializer.java
@@ -4,6 +4,16 @@ package it.unibo.unibodget.persistency.parser.api;
  * Defines the contract for parsing data into a specific type
  * @param <T> the type of the parsed data
  */
-public interface DataParser<T> {
-    T parse(String data) throws DataParserException;
+public interface DataSerializer<T> {
+
+    /**
+     * Serializes the given object into its textual representation.
+     * The returned string should contain a complete and valid
+     * representation of the object
+     *
+     * @param data the object to serialize
+     * @return the textual representation of the object
+     * @throws DataSerializerException if the object cannot be serialized
+     */
+    String serialize(T data) throws DataSerializerException;
 }
