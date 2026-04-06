4d0f5fd8a626b439f8d0bfb8ce7a53e33f76b6a3
Arianna Palmucci <arianna.palmucci@studio.unibo.it>
2026-04-06 12:53:29 +0200

    updated basicreader implementation with path validation - private method to validate path during object construction - validatePath --> to check existence, regularity, or readability - completed comments

--- app/src/main/java/it/unibo/unibodget/persistency/reader/impl/BasicReader.java
+++ app/src/main/java/it/unibo/unibodget/persistency/reader/impl/BasicReader.java
@@ -1,5 +1,8 @@
 package it.unibo.unibodget.persistency.reader.impl;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import it.unibo.unibodget.persistency.reader.api.FileReader;
 
 /**
@@ -10,16 +13,52 @@ import it.unibo.unibodget.persistency.reader.api.FileReader;
 public abstract class BasicReader<T> implements FileReader<T> {
     private final String path;
 
+    /**
+     * Assigning the path of the file to be read
+     * If the path is null, blank, does not exist, is not a regular file,
+     * or cannot be read, an {@link IllegalArgumentException} is thrown
+     * 
+     * @param path the path of the file to be read
+     * @throws IllegalArgumentException if the path is invalid or the file is not accessible
+     */
     protected BasicReader(final String path) {
+        if (path == null || path.isBlank()) {
+            throw new IllegalArgumentException("File path cannot be null or blank");
+        }
         this.path = path;
+        validatePath();
     }
 
     /** 
-     * Returns the path of the file to be read.
-      *
-      * @return the path of the file
+     * Returns the validated path of the file associated with this reader
+     *
+     * @return the path of the file
      */
     protected String getPath() {
         return this.path;
+    }
+
+    /**
+     * Validates the provided file path
+     * Checks if the file exists, is a regular file, and is readable.
+     * 
+     * If any of these conditions are not met, an {@link IllegalArgumentException} 
+     * is thrown with an appropriate message
+     * 
+     * @param path the file path to validate
+     * @throws IllegalArgumentException if the file does 
+     *             not match conditions for existence, regularity, or readability
+     */
+    private void validatePath() {
+        final Path path_to_check = Path.of(path);
+        if (!Files.exists(path_to_check)) {
+            throw new IllegalArgumentException("File does not exist: " + path);
+        }
+        if (!Files.isRegularFile(path_to_check)) {
+            throw new IllegalArgumentException("Path is not a file: " + path);
+        }
+        if (!Files.isReadable(path_to_check)) {
+            throw new IllegalArgumentException("File is not readable: " + path);
+        }
     }
 }
