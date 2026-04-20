3fe269e9a3fcc95dfd5fd4f397c9a36caeefbe14
Arianna Palmucci <arianna.palmucci@studio.unibo.it>
2026-04-20 23:21:56 +0200

    upload Asset in model/currency with sign

--- app/src/main/java/it/unibo/unibodget/model/currency/Asset.java
+++ app/src/main/java/it/unibo/unibodget/model/currency/Asset.java
@@ -7,6 +7,7 @@ import java.util.Objects;
  * Represents a financial asset composed of:
  * - an amount
  * - a currency unit (fiat, crypto, stock)
+ * - a sign indicating whether it's an income ('+') or an expense ('-')
  *
  * This class is immutable and ensures that both fields are non-null.
  * It provides getters to access the amount and the associated currency unit.
@@ -17,16 +18,19 @@ public class Asset {
 
     private CurrencyUnit currency;
     private BigDecimal amount;
+    private final char sign;
 
     /**
      * Creates a new Asset with the given currency and amount.
      *
      * @param currency the currency unit of the asset; must not be null
      * @param amount   the numerical amount; must not be null
+     * @param sign     the sign of the amount ('+' or '-')
      */
-    public Asset(CurrencyUnit currency, BigDecimal amount) {
+    public Asset(CurrencyUnit currency, BigDecimal amount, char sign) {
         this.currency = currency;
         this.amount = amount;
+        this.sign = sign;
     }
 
     /**
@@ -47,14 +51,26 @@ public class Asset {
         return this.currency;
     }
 
+    /**
+     * Returns the sign of this asset,
+     * indicating whether it's an income ('+') or an expense ('-').
+     *
+     * @return the sign character
+     */
+    public char getSign() {
+        return this.sign;
+    }
+
     @Override
     public String toString() {
-        return "Asset{amount=" + this.amount + ", currency=" + this.currency.getShortName() + "}";
+        return "Asset{amount=" + this.amount + 
+                ", currency=" + this.currency.getShortName() + 
+                ", sign='" + this.sign + "'}";
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.amount, this.currency.getCode());
+        return Objects.hash(this.amount, this.currency.getCode(), this.sign);
     }
 
     @Override
@@ -63,7 +79,8 @@ public class Asset {
         if (!(obj instanceof Asset)) return false;
         Asset other = (Asset) obj;
         return Objects.equals(this.amount, other.amount)
-                && Objects.equals(this.currency.getCode(), other.currency.getCode());
+                && Objects.equals(this.currency.getCode(), other.currency.getCode())
+                && this.sign == other.sign;
     }
 
 }
