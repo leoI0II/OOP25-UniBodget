950e53cde5b60c92906a17b3aca28c77e0ce585b
Favour Osaretin Emuwahen <favour.emuwahen@studio.unibo.it>
2026-04-30 16:25:00 +0200

    refactor: remove obsolete movement history layer

--- app/src/main/java/it/unibo/unibodget/model/dashboard/support/TransactionObserver.java
+++
@@ -1,8 +0,0 @@
-package it.unibo.unibodget.model.dashboard.support;
-
-public interface TransactionObserver {
-    /**
-     * Performs the action associated with a movement history update.
-     */
-    void update();
-}
