876614d1189fad596cf026a332f14a3a369505a3
Arianna Palmucci <arianna.palmucci@studio.unibo.it>
2026-05-06 18:12:52 +0200

    add initialized CurrencyHistoryChartView class for values graph

---
+++ app/src/main/java/it/unibo/unibodget/view/currency_converter/CurrencyHistoryChartView.java
@@ -0,0 +1,6 @@
+package it.unibo.unibodget.view.currency_converter;
+
+// CurrencyHistoryChartView per costruire un grafico usando i dati di CurrencyHistoryPoint
+public class CurrencyHistoryChartView {
+    
+}
