816a11e4ab8473fe2ac298256c679a9779d80338
Daniele Zappala <daniele.zappala3@studio.unibo.it>
2026-05-23 15:59:27 +0200

    add: mockup App class that load the side bar and investment page to test the UI

---
+++ app/src/main/java/it/unibo/unibodget/App.java
@@ -0,0 +1,75 @@
+package it.unibo.unibodget;
+
+import it.unibo.unibodget.model.converter.provider.MockExchangeRateProvider;
+import it.unibo.unibodget.model.converter.provider.MockPriceProvider;
+import it.unibo.unibodget.model.currency.FiatCurrency;
+import it.unibo.unibodget.model.investment.controllers.DefaultInvestmentController;
+import it.unibo.unibodget.model.investment.service.CSVInvestmentsSnapshotService;
+import it.unibo.unibodget.model.service.CashAccountService;
+import it.unibo.unibodget.model.service.InvestmentAccountService;
+import it.unibo.unibodget.model.settings.Settings;
+import it.unibo.unibodget.model.wallet.InvestmentAccount;
+import it.unibo.unibodget.view.investments.InvestmentsViewController;
+import it.unibo.unibodget.view.main.SideBarViewController;
+import it.unibo.unibodget.view.main.ViewControllersFactory;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        var mockProvider = new MockExchangeRateProvider();
+        var investmentsService = new InvestmentAccountService();
+        var cashAccountsService = new CashAccountService();
+        var settings = new Settings();
+        var snapshotService = new CSVInvestmentsSnapshotService();
+        var investmentsController = new DefaultInvestmentController(
+                investmentsService,
+                cashAccountsService,
+                mockProvider,
+                settings,
+                snapshotService
+        );
+        investmentsService.addWallet(new InvestmentAccount("Binance", FiatCurrency.USD, new MockExchangeRateProvider()));
+        investmentsService.addWallet(new InvestmentAccount("OKX", FiatCurrency.USD, new MockExchangeRateProvider()));
+
+        FXMLLoader sideBarLoader = new FXMLLoader(getClass()
+                .getResource("/it/unibo/unibodget/view/jfx/fxml/main/SideBarView.fxml")
+        );
+        Node sideBar = sideBarLoader.load();
+        SideBarViewController sideBarVC = sideBarLoader.getController();
+
+        FXMLLoader investmentsPageLoader = new FXMLLoader(
+                getClass().getResource(
+                        "/it/unibo/unibodget/view/jfx/fxml/investments/InvestmentsView.fxml"
+                )
+        );
+        var factory = new ViewControllersFactory(investmentsController, snapshotService);
+        investmentsPageLoader.setControllerFactory(factory::create);
+        Node investmentsPageView = investmentsPageLoader.load();
+
+        // collegamento side bar
+        InvestmentsViewController investmentsVC = investmentsPageLoader.getController();
+        investmentsVC.setSideBarViewController(sideBarVC);
+        sideBarVC.setDelegate(investmentsVC);
+        investmentsVC.onItemSelected(investmentsController.getAllInvestmentAccounts().getFirst().getId());
+
+        // metto tutto in un Hbox temp
+        var root = new HBox(sideBar, investmentsPageView);
+        HBox.setHgrow(investmentsPageView, Priority.ALWAYS);
+
+        primaryStage.setScene(new Scene(root, 900, 700));
+        primaryStage.setTitle("Investments");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
