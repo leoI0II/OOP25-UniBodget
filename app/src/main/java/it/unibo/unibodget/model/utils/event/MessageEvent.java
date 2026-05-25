24ed15e838867743046c4178921cc3b0da832169
Daniele Zappala <daniele.zappala3@studio.unibo.it>
2026-05-25 02:41:42 +0200

    add: javadoc for the marker interface MessageEvent

--- app/src/main/java/it/unibo/unibodget/model/utils/event/MessageEvent.java
+++ app/src/main/java/it/unibo/unibodget/model/utils/event/MessageEvent.java
@@ -1,3 +1,11 @@
 package it.unibo.unibodget.model.utils.event;
 
+/**
+ * Marker interface for all events published through {@link it.unibo.unibodget.model.utils.MessageBus}.
+ *
+ * <p>Implement this interface to define a new event type. Records are recommended:</p>
+ * <pre>{@code
+ * public record BudgetCreated(BigDecimal amount) implements MessageEvent {}
+ * }</pre>
+ */
 public interface MessageEvent {}
