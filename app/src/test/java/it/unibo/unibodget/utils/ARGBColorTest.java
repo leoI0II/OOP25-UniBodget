26be393ff5685fbd72cd27e29755b29a9af59b56
Arianna Palmucci <arianna.palmucci@studio.unibo.it>
2026-07-26 17:50:59 +0200

    removed import not used

--- app/src/test/java/it/unibo/unibodget/utils/ARGBColorTest.java
+++ app/src/test/java/it/unibo/unibodget/utils/ARGBColorTest.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.time.zone.ZoneRules;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
